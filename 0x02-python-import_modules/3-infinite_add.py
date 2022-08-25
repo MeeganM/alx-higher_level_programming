44c65a2e73f44ebf90f4621442809cbca321d8a5
MeeganM <mositoameegan@gmail.com>
2022-08-25 13:17:48 -0700

    0x02. Python - import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+            print("{:d}".format(add))
+
+
 if __name__ == "__main__":
     import sys
-
-    sum = 0
-    for arg in range(len(sys.argv) - 1):
-        sum += int(sys.argv[arg + 1])
-        print("{}".format(sum))
+    add_arg(sys.argv)
