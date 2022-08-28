174bdf912cea4186d8a3aef8d9c86130d163c255
MeeganM <mositoameegan@gmail.com>
2022-08-28 05:20:30 -0700

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    '''comandline arguments'''
     import sys
+    i = len(sys.argv) - 1
 
-    count = len(sys.argv)
-    count -= 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
+    if i == 0:
+        print("{} arguments.".format(i))
+    elif i == 1:
+        print("{} argument:".format(i))
     else:
-        print("{} arguments:".format(count))
-        for i in range(1, count + 1):
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{} arguments:".format(i))
+
+        if i >= 1:
+            i = 0
+            for arg in sys.argv:
+                if i != 0:
+                    print("{}: {}".format(i, arg))
+                    i += 1
